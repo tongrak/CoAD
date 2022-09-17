3b25efe5067820dfe9d5436a8264477afbd21702
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-09-17 20:49:39 +0700

    java foothold

---
+++ AssemblerInt.java
@@ -0,0 +1,3 @@
+public interface AssemblerInt {
+    void interpretAndSave(String [] pc);
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+  private String pc [] ;
+  private String mem [] = new String[10];
+  private String reg [] = new String[8] ;
+
+
+  private void initial(){
+    // AssemblerInt
+  }
+
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+    }
+  }
