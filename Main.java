c286f324077afd3accbb7a94c643507252562055
tanhatai159 <gun159@outlook.com>
2022-09-24 16:19:15 +0700

    implementing Main

--- Main.java
+++ Main.java
@@ -1,14 +1,31 @@
+import assembler.Assembler;
+import computer.Computer;
+import machine.Machine;
+
 public class Main {
 
+  private Assembler assembler;
+  private Machine machine;
+  private Computer computer;
+  public String fileDst;
 
-  private void initial(){
+  public void initial(){
     // AssemblerInt
     // MachineInt  
+    assembler = new Assembler();
+    machine = new Machine();
+    computer = new Computer();
   }
 
+  public void runProgram(){
+    initial();
+    assembler.interpretAndSave(fileDst, computer);
 
+    while(!computer.getEnd()){
 
+    }
+  }
     public static void main(String[] args) {
-      System.out.println("Hello World");
+      Main mainProgram = new Main();
     }
   }
