0dfa16c902be1a312258c16c07104814bbfb166d
Kittiphum <76035271+kittiphum717@users.noreply.github.com>
2022-09-30 17:19:29 +0700

    Add JType and OType

--- aux_use/Helper.java
+++ aux_use/Helper.java
@@ -1,18 +1,22 @@
 package aux_use;
+
 import java.lang.Math;
 
 public class Helper {
-    public static int binToInt(String bin)
-    {
+    public static int binToInt(String bin) {
         double res = 0;
-        for(int i=0;i<bin.length();i++)
-        {
+        for (int i = 0; i < bin.length(); i++) {
             char bit = bin.charAt(i);
-            if(bit == '1')
-            {
-                res = res + Math.pow(2, bin.length()-i-1);
+            if (bit == '1') {
+                res = res + Math.pow(2, bin.length() - i - 1);
             }
         }
         return (int) res;
     }
+
+    public static String IntTobin(int inT) {
+
+        return "";
+    }
+
 }
--- machine/Machine.java
+++ machine/Machine.java
@@ -9,30 +9,33 @@ import computer.ComputerInt;
 public class Machine {
     private static ComputerInt com;
 
-    public static void read_store(ComputerInt comRead)
-    {
+    public static void read_store(ComputerInt comRead) {
         com = comRead;
     }
 
-    public static void Inst_compute(String inst)
-    {
+    public static void Inst_compute(String inst) {
         String opcode = inst.substring(7, 10);
-        if(opcode.equals("000") || opcode.equals("001"))
-            RType(inst,opcode);
+        if (opcode.equals("000") || opcode.equals("001")) {
+            RType(inst, opcode);
+        } else if (opcode.equals("101")) {
+            JType(inst, opcode);
+        } else {
+            OType(inst, opcode);
+        }
+
     }
 
-    public static void RType(String instruction,String opcode) {
+    public static void RType(String instruction, String opcode) {
         String rs = instruction.substring(10, 13);
         String rt = instruction.substring(13, 16);
         String rd = instruction.substring(29, 32);
 
-        if(! rd.equals("000"))
-        {
+        if (!rd.equals("000")) {
             String rs1 = com.getReg(Helper.binToInt(rs));
             String rs2 = com.getReg(Helper.binToInt(rt));
             String res = "";
             String op = "";
-            if(opcode.equals("000"))
+            if (opcode.equals("000"))
                 op = "add";
             else
                 op = "nand";
@@ -49,13 +52,27 @@ public class Machine {
 
     }
 
-    public void JType(String instruction) {
-        String opcode = instruction.substring(7, 10);
+    public static void JType(String instruction, String opcode) {
         String rs = instruction.substring(10, 13);
         String rt = instruction.substring(13, 16);
+
+        String regA = com.getReg(Helper.binToInt(rs));
+        String regB = com.getReg(Helper.binToInt(rt));
+        int PC = com.getPC() + 1;
+        com.setReg(Helper.binToInt(rt), Helper.IntTobin(PC));
+
+        if (regA.equals(regB)) {
+            com.setPC(PC);
+        } else {
+            com.setPC(Helper.binToInt(rs));
+        }
     }
 
-    public void OType(String instruction) {
-        String opcode = instruction.substring(7, 10);
+    public static void OType(String instruction, String opcode) {
+
+        if (opcode.equals("110")) {
+            com.setPC(com.getPC() + 1);
+            com.setEnd(true);
+        }
     }
 }
